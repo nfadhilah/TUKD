7646a4b58b43bb0be9a963fed07683b18b01b4cf
nfadhilah <halilintar@gmail.com>
2021-11-23 17:17:47 +0800

    Add project files.

---
+++ TUKD.Web/Services/ILoadingService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUKD.Web.Services
+{
+    public interface ILoadingService
+    {
+        bool IsLoading { get; set; }
+        Action? OnLoading { get; set; }
+        void SetLoading();
+        void StopLoading();
+    }
+
+    public class LoadingService : ILoadingService
+    {
+        public Action? OnLoading { get; set; }
+        public bool IsLoading { get; set; }
+
+        public void SetLoading()
+        {
+            IsLoading = true;
+            OnLoading?.Invoke();
+        }
+
+        public void StopLoading()
+        {
+            IsLoading = false;
+            OnLoading?.Invoke();
+        }
+    }
+}
